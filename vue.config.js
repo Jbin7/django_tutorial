e33a2b298ca101ea5d7d375ac8ba3febbe58cdce
김정빈(개인) <kjb7907@naver.com>
2019-03-13 00:59:52 +0900

    vue config

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
     devServer: {
         port: 3000,
     },
-    build: {
-        index: path.resolve(__dirname, '../../src/main/resources/static/index.html'),
-        assetsRoot: path.resolve(__dirname, '../../src/main/resources/static')
-    }
+
+    publicPath: '/',
+    outputDir: 'dist',
+    assetsDir: '',
 }
